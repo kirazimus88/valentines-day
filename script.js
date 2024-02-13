33575ca3aec4a245325441d5ec2fe0b09c31b848
kirazimus88 <159176083+kirazimus88@users.noreply.github.com>
2024-02-13 15:03:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "Вы разибил мне сердце! Я плачу..";
+      "Вы разибил мне сердце! Я плачу..(эта кнопка для визуала)";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
